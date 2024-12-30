36cf45af891a653ae7cef0f64d1ea492d7c804ed
1121 <54799827+gmjhmgjkhylyhulghjlbjh23@users.noreply.github.com>
2024-12-30 20:19:32 +0100

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose'); // Assicurati che questa riga sia presente e corretta
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
